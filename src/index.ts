d26a33887565a16925099be38569689a2cc9e4ff
Jan Schulte <hello@unexpected-co.de>
2015-05-22 19:47:38 +0200

    restructure project

--- src/index.ts
+++ src/index.ts
@@ -19,12 +19,12 @@ app.on('window-all-closed', function () {
 
 app.on('ready', function () {
 	mainWindow = new BrowserWindow({
-		width: 600,
-		height: 400,
-		resizable: false
+		width: 1024,
+		height: 768,
+		resizable: true
 	});
-
-	mainWindow.loadUrl(`file://${__dirname}/index.html`);
+	mainWindow.openDevTools();
+	mainWindow.loadUrl(`file://${__dirname}/../static/index.html`);
 
 	mainWindow.on('closed', function () {
 		// deref the window
