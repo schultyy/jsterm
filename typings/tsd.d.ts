f908595bdf597e0201082f77943a0fcb0cde2f6f
Jan Schulte <hello@unexpected-co.de>
2015-05-22 20:02:13 +0200

    install more typings

---
+++ typings/tsd.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="backbone/backbone.d.ts" />
+/// <reference path="github-electron/github-electron.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="underscore/underscore.d.ts" />
