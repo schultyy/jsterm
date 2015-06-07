97df921de0c93e287aa8cae8a431739d6c16e44d
Jan Schulte <hello@unexpected-co.de>
2015-06-07 18:35:29 +0200

    remove requirejs definition

--- typings/tsd.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="jquery/jquery.d.ts" />
-/// <reference path="backbone/backbone.d.ts" />
-/// <reference path="github-electron/github-electron.d.ts" />
-/// <reference path="node/node.d.ts" />
-/// <reference path="underscore/underscore.d.ts" />
-/// <reference path="requirejs/require.d.ts" />
